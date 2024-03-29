e6d0258ce5e7b5461ce93ff05f4034170eb3ecc9
duyhung753951 <148344213+duyhung753951@users.noreply.github.com>
2024-03-30 00:26:59 +0700

    Update Bai_3_c.cpp

--- Bai_3_c.cpp
+++ Bai_3_c.cpp
@@ -6,14 +6,18 @@ using namespace std;
 float fx(float x){
     return (2*x*cos(2*x) - pow(x+1,2));
 }
+float giaipt(float a, float b);
 
 int main()
 {
-    float a, b, e, x0, x;
-    
-    a = -1;
-    b = 0;
-    e = 0.000001;
+    cout << "nghiem x1 = " << giaipt(-3,2) << endl;
+    cout << "nghiem x2 = " << giaipt(-1,0);
+    return 0;
+}
+
+float giaipt(float a, float b){
+    float e = 0.0000001;
+    float x0;
     while((b-a)>=e){
         x0=(a+b)/2;
         if(fx(a)*fx(x0) > 0){
@@ -22,8 +26,5 @@ int main()
             b = x0;
         }
     }
-    cout << x0;
-    
-    return 0;
+    return x0;
 }
-
